91ca9ff7e9d9f6f5d103faaa999a2b8cad905ab6
Mishal Alsharekh <mishal.sharekh@gmail.com>
2018-11-22 21:29:12 +0300

    complete GET and POST points

--- BlockController.js
+++ BlockController.js
@@ -22,11 +22,30 @@ class BlockController {
      * Implement a GET Endpoint to retrieve a block by index, url: "/api/block/:index"
      */
     getBlockByIndex() {
-        this.app.get("/api/block/:index", (req, res) => {
+        this.app.get("/api/block/:blockheight", (req, res) => {
             // Add your code here
-            console.log('visiting getBlockByIndex');
-            var obj=req.params
-            console.log(obj.index);
+            var blockheight=req.params.blockheight;
+            
+            // console.log(`getBlockByBlockheight req blockheight= ${blockheight}`);
+            //Mock Data
+            var blocks = [];
+            let block_1 = {"height":"0","body":"Udacity Blockchain Developer", "time": 1538509789};
+            let block_2 = {"height":"1","body":"Udacity Blockchain Developer Rock!", "time": 1538509789};
+            let block_3 = {"hash":"ffaffeb2330a12397acc069791323783ef1a1c8aab17ccf2d6788cdab0360b90","height":1,"body":"Testing block with test string data","time":"1531764891","previousBlockHash":"49cce61ec3e6ae664514d5fa5722d86069cf981318fc303750ce66032d0acff3"}
+            let block_4 = {"hash":"49cce61ec3e6ae664514d5fa5722d86069cf981318fc303750ce66032d0acff3","height":0,"body":"First block in the chain - Genesis block","time":"1530311457","previousBlockHash":""}
+
+            blocks.push(block_1);
+            blocks.push(block_2);
+            blocks.push(block_3);
+            blocks.push(block_4);
+
+ 
+            var block=JSON.stringify(blocks[blockheight]);
+            // console.log(`getBlockByBlockheight res block= ${block}`);
+            // console.log(`getBlockByBlockheight res block= ${JSON.parse(block).body}`);
+            
+            res.send(block);
+
 
         });
     }
@@ -36,8 +55,19 @@ class BlockController {
      */
     postNewBlock() {
         this.app.post("/api/block", (req, res) => {
-             console.log('visiting postNewBlock');
+            //  console.log('visiting postNewBlock req= '+req.body);
+            //  console.log('visiting postNewBlock res= '+res);
             // Add your code here
+            console.log(req.body);
+            // var blockbody=JSON.parse(req);
+            
+            var obj={
+                type:"POST",
+                title:req.body.title,
+                height:req.body.height
+            };
+            res.send(obj);
+
         });
     }
 
