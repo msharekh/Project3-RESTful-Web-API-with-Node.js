24d5ecbb85a80a5ba468d5eaaf22080f29790ac0
Mishal Alsharekh <mishal.sharekh@gmail.com>
2018-11-23 00:30:41 +0300

    getBlock from BlockChainClass db

--- BlockChain.js
+++ BlockChain.js
@@ -403,3 +403,5 @@ class BlockChain{
     
   }//<-------end BlockChain
   
+
+  module.exports.BlockChain = BlockChain;
--- BlockController.js
+++ BlockController.js
@@ -32,38 +32,20 @@ class BlockController {
             var blockheight=req.params.blockheight;
             
 
-            this.initializeMockData();
+            // this.initializeMockData();
 
-            this.blocks[blockheight];
+            // this.blocks[blockheight];
 
             // console.log(`getBlockByBlockheight req blockheight= ${blockheight}`);
-            //Mock Data
-            // var blocks = [];
-            // let block_1 = {"height":"0","body":"Udacity Blockchain Developer", "time": 1538509789};
-            // let block_2 = {"height":"1","body":"Udacity Blockchain Developer Rock!", "time": 1538509789};
-            // let block_3 = {"hash":"ffaffeb2330a12397acc069791323783ef1a1c8aab17ccf2d6788cdab0360b90","height":1,"body":"Testing block with test string data","time":"1531764891","previousBlockHash":"49cce61ec3e6ae664514d5fa5722d86069cf981318fc303750ce66032d0acff3"}
-            // let block_4 = {"hash":"49cce61ec3e6ae664514d5fa5722d86069cf981318fc303750ce66032d0acff3","height":0,"body":"First block in the chain - Genesis block","time":"1530311457","previousBlockHash":""}
-
-            // blocks.push(block_1);
-            // blocks.push(block_2);
-            // blocks.push(block_3);
-            // blocks.push(block_4);
-
-
-
-            // let bc = new BlockChainClass.BlockChain();
-
-            // bc.getBlock(blockheight).then((b) => {
-            //             c(JSON.parse(b))
-            // })
- 
-            var block=JSON.stringify(this.blocks[blockheight]);
-            console.log(`getBlockByBlockheight res block= ${block}`);
-            console.log(`getBlockByBlockheight res block= ${JSON.parse(block).body}`);
             
-            res.send(block);
 
+            let bc = new BlockChainClass.BlockChain();
 
+            var obj =bc.getBlock(blockheight).then((b) => {
+                        c(JSON.parse(b))
+                        res.send(JSON.parse(b));
+            })
+ 
         });
     }
 
